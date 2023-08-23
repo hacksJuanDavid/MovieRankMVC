85973cf67131c16a3ca5909a159919abefb6db83
etensor <dpenillac@correo.usbcali.edu.co>
2023-08-23 00:00:53 -0500

    models base, agregar si algo a movies mas columnas melas

---
+++ Models/Movie.cs
@@ -0,0 +1,36 @@
+namespace MovieRank.Models;
+
+public class Movie
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Title is required")]
+    [StringLength(50)]
+    public string Title { get; set; } = null!;
+
+    [Required(ErrorMessage = "A Synopsis is required")]
+    [StringLength(300)]
+    public string Synopsis { get; set; } = null!;
+
+    [Required(ErrorMessage = "Release year is required")]
+    public int Year { get; set; }
+
+    [Required(ErrorMessage = "The duration is required")]
+    [StringLength(5)]
+    // Format hh:mi
+    public string Duration { get; set; } = null!;
+
+    // [Required(ErrorMessage = "Rating should not be empty.")]
+    // Format 0.0
+    public float Rate { get; set; }
+
+    [Required(ErrorMessage = "A poster is required.")]
+    [StringLength(80)]
+    // Include only ImageName.ext
+    public string Poster { get; set; } = null!;
+
+    // Optional List separated with pipe (|) ej: Action|Adventure|Sci-fi
+    public string Genres { get; set; } = null!;
+ 
+}
